f2adef32d9390dd240bb4ba47069d5ad84b6f4e8
to4stty <captainbreadcrumbs@proton.me>
2026-05-24 01:20:45 -0700

    second commit

---
+++ conditional_responder.py
@@ -0,0 +1,23 @@
+def add_user_name():
+    user_name = []
+    
+    user_name = input("What is your name? : ")
+    if not user_name :
+        print("Name can not be empty.")
+
+    print(f"Hello {user_name}. How are you?")
+    mood = input("").lower().strip()
+    if mood == "i am fine":
+        print("That is good to hear.")
+
+    elif mood == "i am not fine":
+        print("What happened? And could i help you with something?")
+        mood_answer = input("").lower().strip()
+
+        if mood_answer == "no":
+            print("Sorry to hear that, I hope it gets better soon")
+
+        elif mood_answer == "yes":
+            print("Great, how can I help you?")
+
+add_user_name()
--- exceptionhandling_return.py
+++
@@ -1,8 +0,0 @@
-while True:
-    user_name = input("You : ")
-    if user_name == "exit":
-        print("Goodbye!")
-        break
-
-    else:
-        print(f"Bot : Hello {user_name}")
